20c8b4a5949bbfe47961a1d529fccc0fa650200f
Leanne <leanne.tite@gmail.com>
2014-08-07 13:36:53 +0100

    GumCal: new Handles notEquals condition; status badges now only show when status count is not 0

--- js/lib/handlebars-helpers.js
+++ js/lib/handlebars-helpers.js
@@ -4,3 +4,10 @@ Handlebars.registerHelper('ifCondition', function(v1, v2, options) {
   }
   return options.inverse(this);
 });
+
+Handlebars.registerHelper('ifNotEquals', function(v1, v2, options) {
+  if(v1 !== v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
