1e7a1810b161fe48cc563decd7c7ed2a39147e42
leanne <leanne.tite@gmail.com>
2014-11-02 21:00:13 +0000

    Register file delete

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.configure( function() {
     app.use( app.router );
 
     //Where to serve static content
-    app.use( express.static( path.join( application_root, '../templates') ) );
+    app.use( express.static( path.join( application_root, '../site') ) );
 
     //Show all errors in development
     app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
